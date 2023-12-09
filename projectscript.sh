fa355be2e4ba71ca956f5f80b9811dc850ef199d
ahmednasserhu <nasserjr@localhost.localdomain>
2023-12-09 23:45:09 +0200

    drop database featurue added

--- projectscript.sh
+++ projectscript.sh
@@ -20,13 +20,24 @@ else
 	case $choice in
         createDB)
 	find / -name createdb.sh 2>/dev/null | while read -r file; do cp "$file" .; done
-	cd ./DB
+	cd ./DB 2>/dev/null
 	source ../createdb.sh
 	;;
 	listDB)
 	cd ./DB
 	echo `ls -F|grep '/'`
 	;;
+	dropBD)
+		read -p "Enter the name of the database that you want to delete: " db
+		path=./DB/$db
+		if [ -d "$path" ]
+		then
+		rm -r "$path"
+		echo "Database '$db' deleted successfully."
+		else
+		echo "Invalid name. Please enter the correct name."
+		fi
+	;; 
 	exit)
 	exit
 	;;
