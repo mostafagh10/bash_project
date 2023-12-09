52ac33b2a9b303f61f26c154f11938b4ff6d0215
mostafagh10 <ghazalymostafa07@gmail.com>
2023-12-09 18:37:43 +0200

    add DB dir

---
+++ projectscript.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+shopt -s extglob
+
+#script1->check for the DB directory
+flag1=0
+for var in `ls -F|grep '/'`
+do
+if [ $var = "DB/" ]
+then
+	((flag1=1))
+fi
+done
+if [ $flag1 -eq 0 ]
+then
+	mkdir DB
+	echo "the DB Directory created"
+else
+	echo "show list"
+fi
